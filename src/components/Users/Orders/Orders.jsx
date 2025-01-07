fd3c4342fd1971fc2e3f6ff27e61cbf3e232384b
unknown <joan_mart_her@hotmail.com>
2025-01-07 21:08:01 +0100

    Solve show orders problem

--- src/components/Users/Orders/Orders.jsx
+++ src/components/Users/Orders/Orders.jsx
@@ -1,38 +1,55 @@
 import React, { useContext, useEffect } from 'react'
 import { UserContext } from '../../../context/UserContext/UserState'
-import { Card } from 'antd';
+import { Card, Button, Typography } from 'antd';
+const { Text, Paragraph, Title } = Typography;
 import './Orders.scss'
 
 const { Meta } = Card;
 
 const Orders = () => {
     const {getUserInfo, user} = useContext(UserContext)
+    let total = 0
 
     useEffect(()=>{
         getUserInfo()        
     },[])
     //Por alguna razón dentro del return no lee el user.Orders(dice que no está definido)
     console.log("Usuario cuando llega pedidos: ",user.Orders);
-    
+
   return (
         <div className='orders'>
-        {/* {user.Orders.map((order) => {
+        {user.Orders?.map((order) => {
             return (
-                <Card
-                    key={order.id}
-                    hoverable
-                    style={{
-                      width: 240,
-                    }}
-                    cover={<img alt="img order" src="https://d1gl66oyi6i593.cloudfront.net/wp-content/uploads/2020/07/venta-honda-nsx-type-r-1993.jpg" />}
-                  >
-                    <Meta title={order.description} description={"Productos: " } />
-                    <Button type="primary" onClick={()=>addCart(order)}>Comprar</Button>
-                </Card>
+                  <div className='orders'>
+                    <Card
+                        key={order.id}
+                        hoverable
+                        //onClick={editOrder}
+                        style={{
+                          width: 200,
+                        }}
+                        cover={<img alt="img coche" src="https://cdn-icons-png.flaticon.com/512/7132/7132915.png" />}
+                      >
+                        <Meta title={order.description} description={"Productos: " + order.Products.map((product)=>{total += +product.price 
+                          return(" "+product.name + " ("+ product.price + "€)")})} />
+                        <div className='total'>Total: {total}€</div> 
+                        <Button type="primary" 
+                          style={{
+                            width: 70
+                          }}
+                        //onClick={()=>addCart(product)}
+                        >Comprar</Button>
+                        <Button type="primary" danger
+                        style={{
+                          width: 70
+                        }}
+                        //onClick={()=>addCart(product)}
+                        >Eliminar</Button>
+                    </Card>
+                  </div>
             )
-        })} */}
-        Trabajando en ello
-    </div>
+        })}
+   </div>
     
   )
 }
