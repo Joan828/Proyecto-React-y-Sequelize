be5756dc13259e2ecb19292d537bac9d0648d674
unknown <joan_mart_her@hotmail.com>
2025-01-08 13:36:53 +0100

    Total and title order problems fixed

--- src/components/Users/Orders/Orders.jsx
+++ src/components/Users/Orders/Orders.jsx
@@ -1,24 +1,21 @@
 import React, { useContext, useEffect } from 'react'
 import { UserContext } from '../../../context/UserContext/UserState'
-import { Card, Button, Typography } from 'antd';
-const { Text, Paragraph, Title } = Typography;
+import { Card, Button } from 'antd';
 import './Orders.scss'
 
 const { Meta } = Card;
 
 const Orders = () => {
     const {getUserInfo, user} = useContext(UserContext)
-    let total = 0
 
     useEffect(()=>{
         getUserInfo()        
     },[])
-    //Por alguna razón dentro del return no lee el user.Orders(dice que no está definido)
-    console.log("Usuario cuando llega pedidos: ",user.Orders);
 
   return (
         <div className='orders'>
-        {user.Orders?.map((order) => {
+        {user.Orders?.map((order,i) => {
+          order["total" + order.id] = 0          
             return (
                   <div className='orders'>
                     <Card
@@ -30,9 +27,10 @@ const Orders = () => {
                         }}
                         cover={<img alt="img coche" src="https://cdn-icons-png.flaticon.com/512/7132/7132915.png" />}
                       >
-                        <Meta title={order.description} description={"Productos: " + order.Products.map((product)=>{total += +product.price 
+                        <Meta title={"Nº Pedido: " + order.id} description={"Productos: " + order.Products.map((product)=>{
+                           order["total"+order.id]  += +product.price 
                           return(" "+product.name + " ("+ product.price + "€)")})} />
-                        <div className='total'>Total: {total}€</div> 
+                        <div className='total'>Total: { order["total"+order.id]}€</div>                    
                         <Button type="primary" 
                           style={{
                             width: 70
